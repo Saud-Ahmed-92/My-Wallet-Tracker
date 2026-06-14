b0b41d19db48a813dbfdfcebb5ea8f248ad4f7a0
Saud Ahmed <saudahmedkhan2005@gmail.com>
2026-06-15 02:41:48 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,20 @@
-const CACHE = 'paisa-v1';
-const FILES = ['./'];
+const CACHE = 'mywallet-v1';
+const ASSETS = ['./index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(clients.claim());
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./')))
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
   );
 });
